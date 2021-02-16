ccb341e780d3b895fd37b9ddde5342b0e34eaf84
Andrew Burke <Drewburke39@gmail.com>
2021-02-15 20:57:09 -0700

    assigned proper tags to script.js

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 var hours = [9, 10, 11, 12, 1, 2, 3, 4, 5];
 var militaryTime = [09, 10, 11, 12, 13, 14, 15, 16, 17];
-var newRow = "";
+var hour = moment().format('MMMM Do YYYY, h:mm:ss a');
+var currentHour = moment().format('HH');
+console.log(currentHour)
+var index = 0;
+function calander() {
+    console.log("calanderfired")
+
+    for (let index = 0; index < militaryTime.length; index++) {
+
+
+
+
+        if (militaryTime[index] < currentHour) {
+            document.getElementById(militaryTime[index]).classList.add("past")
+
+            console.log(militaryTime[index])
+        } else if (currentHour < militaryTime[index]) {
+            document.getElementById(militaryTime[index]).classList.add("future")
+
+        } else {
+            document.getElementById(militaryTime[index]).classList.add("present")
+
+        }
+    }
+}
+
+
+calander()
